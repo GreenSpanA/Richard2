b1865cef3a888666fee023d3aef28684602a03ac
GreenSpanA <vladimir.davidko@phystech.edu>
2019-01-17 22:01:21 +0300

    Add reading data from csv

--- Richard2/Controllers/HomeController.cs
+++ Richard2/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Richard2.Models;
+using System.IO;
 
 namespace Richard2.Controllers
 {
@@ -20,6 +21,40 @@ namespace Richard2.Controllers
             ViewData["Message"] = "Your application description page.";
 
             return View();
+        }
+
+        
+        public ActionResult SomeActionMethod()
+        {
+            //var employees = DataRepository.GetEmployees();
+
+            ////"Business logic" methog that filter employees by the employer id
+            //var companyEmployees = (from e in employees
+            //                        where (CompanyID == null || e.CompanyID == CompanyID)
+            //                        select e).ToList();
+
+            ////UI processing logic that filter company employees by name and paginates them
+            //var filteredEmployees = (from e in companyEmployees
+            //                         where (param.sSearch == null || e.Name.ToLower().Contains(param.sSearch.ToLower()))
+            //                         select e).ToList();
+            //var result = from emp in filteredEmployees.Skip(param.iDisplayStart).Take(param.iDisplayLength)
+            //             select new[] { Convert.ToString(emp.EmployeeID), emp.Name, emp.Position };
+
+            //var companies = DataRepository.GetCompanies();
+            var companies = System.IO.File.ReadAllLines("Models\\Data\\company_list.csv")
+                                           .Skip(1)
+                                           .Select(v => new Company(v))
+                                           .ToList();
+
+            //var result = companies;
+            var result = from emp in companies select new[] {Convert.ToString(emp.ID), Convert.ToString(emp.Name), emp.Address, emp.Town};
+            return Json(new
+            {
+              
+                iTotalRecords = companies.Count,
+                iTotalDisplayRecords = companies.Count,
+                aaData = result
+            });
         }
 
         public IActionResult Contact()
@@ -39,5 +74,34 @@ namespace Richard2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult MasterDetailsAjaxHandler(JQueryDataTableParamModel param, int? CompanyID)
+        {
+
+            var employees = DataRepository.GetEmployees();
+
+            //"Business logic" methog that filter employees by the employer id
+            var companyEmployees = (from e in employees
+                                    where (CompanyID == null || e.CompanyID == CompanyID)
+                                    select e).ToList();
+
+            //UI processing logic that filter company employees by name and paginates them
+            var filteredEmployees = (from e in companyEmployees
+                                     where (param.sSearch == null || e.Name.ToLower().Contains(param.sSearch.ToLower()))
+                                     select e).ToList();
+            var result = from emp in filteredEmployees.Skip(param.iDisplayStart).Take(param.iDisplayLength)
+                         select new[] { Convert.ToString(emp.EmployeeID), emp.Name, emp.Position };
+
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = companyEmployees.Count,
+                iTotalDisplayRecords = filteredEmployees.Count,
+                aaData = result
+            });
+        }
+
+
     }
 }
+
---
+++ Richard2/Models/Company.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Richard2.Models
+{
+    public class Company
+    {
+        //static int nextID = 17;
+
+        //public Company()
+        //{
+        //    ID = nextID++;
+        //}
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+
+        public Company (string line) {
+            var split = line.Split(';');
+            ID = Convert.ToInt32(split[0]);
+            Name = split[1];
+            Address = split[2];
+            Town = split[3];
+        }
+        //public static Company FromCsv(string csvLine)
+        //{
+        //    string[] values = csvLine.Split(',');
+        //    Company company = new Company();
+        //    company.ID = Convert.ToInt32(values[0]);
+        //    company.Name = values[1];
+        //    company.Address = values[2];
+        //    company.Town = values[3];
+        //    return company;
+        //}
+    }
+}
